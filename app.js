a5ae1444c66d8f89b45aaba868c5199aeed41138
sumy mohanarasa <m.sumangalya@gmail.com>
2016-11-08 17:55:30 +0000

    search function

--- app.js
+++ app.js
@@ -10,14 +10,36 @@ window.PhotoSearch = {
 		opt=opt || {};
 		this.config.client_id = opt.client_id;
 	},
-
+https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=ACCESS_TOKEN
 	popular: function (){
+			var endpoint = this.BASE_URL + '/media/popular?client_id=' + name + '/media/recent?client_id=' + this.config.client_id;
+		this.getJSON (endpoint, callback);
+
 
 	},
 
-	tagsByName: function(name){
-		this.BASE_URL + '//tags/nofilter/media/recent?access_token=ACCESS_TOKEN'
+	/**
+	*Get a list of recently tagged media.
+	*/
+
+	tagsByName: function(name, callback){
+		var endpoint= this.BASE_URL + '/tags/' + name + '/media/recent?client_id=' + this.config.client_id;
+		this.getJSON (endpoint, callback);
+	},
+
+
+	getJSON: function (url, callback ){
+		$.ajax({
+			type: 'GET',
+			url: url,
+			dataType:'jsonp'
+			success: function (response){
+				if (typeof callback === 'function') callback(response);
+
+			}
+		});
 	}
+};
 
 	search
 };
